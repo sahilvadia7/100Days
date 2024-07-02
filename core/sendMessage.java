8b283a6577ed33fe7f5a11540adaced897899ff5
sahil <sahilvadia7776@gmail.com>
2024-07-02 17:31:42 +0530

    minor changes

---
+++ core/sendMessage.java
@@ -0,0 +1,15 @@
+package core;
+
+public class sendMessage {
+    void message(){
+        System.out.println("?");
+    }
+
+    public static void main(String[] args) {
+        sendMessage sm = new sendMessage();
+        while(1!=0){
+            sm.message();
+        }
+    }
+
+}
